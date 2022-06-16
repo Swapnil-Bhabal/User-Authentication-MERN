4b7ec4f376ccfb8930fed7b8ca127ead9110fcf5
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-06-16 11:34:10 +0530

    connected with mongoose succesfully.

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,40 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');        //Since the backend and frontend are running on two different ports so extra headers are required which is filled by this cors.
+const mongoose = require('mongoose');
+const User = require('./models/userModel');
+
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());        //if not put this then req.body remains undefined.
 
 
-app.post('/api/register', (req, res) => {
+mongoose.connect('mongodb://localhost:27017/user-login-page');
+
+
+app.post('/api/register', async (req, res) => {
     console.log(req.body);
-    res.send({ status: 'ok' });
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.send({ status: 'ok' });
+    } catch(err) {
+        console.log(err)
+        res.json({ status: 'error', error: 'Duplicate email' });
+    }
+    
+})
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email, password: req.body.password })
+    if (user) {
+        return res.json({ status: 'ok', user: true })
+    } else {
+        return res.json({ status: 'error', user: false });
+    }
 })
 
 app.listen('4000', () => {
